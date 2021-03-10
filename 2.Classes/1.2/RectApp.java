d10ce965ace21120050e92651604c97866e2b96b
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-03-10 10:04:45 -0300

    Update RectApp.java

--- 2.Classes/1.2/RectApp.java
+++ 2.Classes/1.2/RectApp.java
@@ -2,7 +2,7 @@ public class RectApp {
     public static void main (String[] args) {
         Rect r1 = new Rect(1,1, 10,10);
         r1.print();
-		    r1.drag(8,8);
+		r1.drag(8,8);
     }
 }
 class Rect {
@@ -14,14 +14,14 @@ class Rect {
         this.w = w;
         this.h = h;
     }
-	  int area(){
-		  return (w*h);
-	  }
-	  void drag (int dx, int dy){
-		  this.x = this.x + dx;
-		  this.y = this.y + dy;
-		  System.out.format("Nova posicao do retangulo: (%d,%d)\n", this.x, this.y);
-	  }
+	int area(){
+		return (w*h);
+	}
+	void drag (int dx, int dy){
+		this.x = this.x + dx;
+		this.y = this.y + dy;
+		System.out.format("Nova posicao do retangulo: (%d,%d)\n", this.x, this.y);
+	}
 	
     void print () {
         System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\nArea do retangulo: %d \n",

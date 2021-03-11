bd935581cd377169795c5ce79cdbed6568df411b
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-03-10 22:01:38 -0300

    Update PoligonoApp.java

--- 2.Classes/1.1/PoligonoApp.java
+++ 2.Classes/1.1/PoligonoApp.java
@@ -5,20 +5,20 @@ public class PoligonoApp {
     }
 }
 class Poligono {
-	int x, y;
+    int x, y;
     int w, h;
-	int lados;
-	String traco;
+    int lados;
+    String traco;
     Poligono (int x, int y, int w, int h, int lados, String traco) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-		this.lados = lados;
-		this.traco = traco;
-	}
-	void print () {
-		System.out.format("O poligono de %d lados com contorno %s possui tamanho (%d,%d) e esta na posicao (%d,%d).\n",
-		    this.lados, this.traco, this.w, this.h, this.x, this.y);
-	}
+	this.lados = lados;
+	this.traco = traco;
+    }
+    void print () {
+        System.out.format("O poligono de %d lados com contorno %s possui tamanho (%d,%d) e esta na posicao (%d,%d).\n",
+            this.lados, this.traco, this.w, this.h, this.x, this.y);
+    }
 }

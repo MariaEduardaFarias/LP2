800b48f4b257d5b582da0af3828da36bfb433a07
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-03-21 13:44:52 -0300

    Update PackApp.java

--- 2.Classes/3.1/PackApp.java
+++ 2.Classes/3.1/PackApp.java
@@ -14,7 +14,7 @@ class PackApp {
 class PackFrame extends JFrame {
     Rect r1;
     Ellipse e1;
-	QuadCurve q1, q2;
+    QuadCurve q1, q2;
 
     PackFrame () {
         this.addWindowListener (
@@ -28,15 +28,15 @@ class PackFrame extends JFrame {
         this.setSize(350, 350);
         this.r1 = new Rect(50,50, 100,30);
         this.e1 = new Ellipse(50,100, 100,30);
-		this.q1 = new QuadCurve(50,200,45,110,200,200);
-		this.q2 = new QuadCurve(50,200,150,70,200,200);
+	this.q1 = new QuadCurve(50,200,45,110,200,200);
+	this.q2 = new QuadCurve(50,200,150,70,200,200);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
         this.r1.paint(g);
         this.e1.paint(g);
-		this.q1.paint(g);
-		this.q2.paint(g);
+	this.q1.paint(g);
+	this.q2.paint(g);
     }
 }

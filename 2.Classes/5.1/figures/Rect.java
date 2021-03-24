a59a06a744d9022c99f0c923663e555001317c3c
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-03-24 16:55:07 -0300

    Update Rect.java

--- 2.Classes/5.1/figures/Rect.java
+++ 2.Classes/5.1/figures/Rect.java
@@ -5,20 +5,20 @@ import java.awt.*;
 public class Rect {
     private int x, y;
     private int w, h;
-	private int r, g, b;
-	private int Drawr, Drawg, Drawb;
+    private int r, g, b;
+    private int Drawr, Drawg, Drawb;
 
     public Rect (int x, int y, int w, int h, int r, int g, int b, int Drawr, int Drawg, int Drawb) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-		this.r = r;
-		this.g = g;
-		this.b = b;
-		this.Drawr = Drawr;
-		this.Drawg = Drawg;
-		this.Drawb = Drawb;
+	this.r = r;
+	this.g = g;
+	this.b = b;
+	this.Drawr = Drawr;
+	this.Drawg = Drawg;
+	this.Drawb = Drawb;
     }
 
     private void print () {
@@ -28,8 +28,8 @@ public class Rect {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(new Color(this.r,this.g,this.b));
-		g2d.fillRect(this.x,this.y, this.w,this.h);
+	g2d.setColor(new Color(this.r,this.g,this.b));
+	g2d.fillRect(this.x,this.y, this.w,this.h);
         g2d.setColor(new Color(this.Drawr,this.Drawg,this.Drawb));
         g2d.drawRect(this.x,this.y, this.w,this.h);
     }

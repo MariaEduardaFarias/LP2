256137ae8c3ba51bf77f4533c6692647a9198b52
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-04-01 20:16:52 -0300

    Update Ellipse.java

--- 2.Classes/6.1/figures/Ellipse.java
+++ 2.Classes/6.1/figures/Ellipse.java
@@ -6,20 +6,20 @@ import java.awt.geom.*;
 public class Ellipse extends Figure {
     private int x, y;
     private int w, h;
-	private int r, g, b;
-	private int Drawr, Drawg, Drawb;
+    private int r, g, b;
+    private int Drawr, Drawg, Drawb;
 	
     public Ellipse (int x, int y, int w, int h, int r, int g, int b, int Drawr, int Drawg, int Drawb) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-		this.r = r;
-		this.g = g;
-		this.b = b;
-		this.Drawr = Drawr;
-		this.Drawg = Drawg;
-		this.Drawb = Drawb;
+	this.r = r;
+	this.g = g;
+	this.b = b;
+	this.Drawr = Drawr;
+	this.Drawg = Drawg;
+	this.Drawb = Drawb;
     }
 
     private void print () {
@@ -29,10 +29,10 @@ public class Ellipse extends Figure {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(new Color(this.r,this.g,this.b));
-		g2d.fillOval(this.x,this.y, this.w,this.h);
+	g2d.setColor(new Color(this.r,this.g,this.b));
+	g2d.fillOval(this.x,this.y, this.w,this.h);
         g2d.drawOval(this.x,this.y, this.w,this.h);
-		g2d.setColor(new Color(this.Drawr,this.Drawg,this.Drawb));
-		g2d.drawOval(this.x,this.y, this.w,this.h);
+	g2d.setColor(new Color(this.Drawr,this.Drawg,this.Drawb));
+	g2d.drawOval(this.x,this.y, this.w,this.h);
     }
 }

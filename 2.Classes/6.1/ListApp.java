1becc75f82ab45b3b3ce16ad337aec75eba3418d
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-04-01 19:59:50 -0300

    Add files via upload

---
+++ 2.Classes/6.1/ListApp.java
@@ -0,0 +1,72 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+import figures.*;
+
+class ListApp {
+    public static void main (String[] args) {
+        ListFrame frame = new ListFrame();
+        frame.setVisible(true);
+    }
+}
+
+class ListFrame extends JFrame {
+    ArrayList<Figure> figs = new ArrayList<Figure>();
+    Random rand = new Random();
+
+    ListFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+
+        this.addKeyListener (
+            new KeyAdapter() {
+                public void keyPressed (KeyEvent evt) {
+					int x = rand.nextInt(350);
+                    int y = rand.nextInt(350);
+                    int w = rand.nextInt(50);
+                    int h = rand.nextInt(50);
+					int r = rand.nextInt(255);
+                    int g = rand.nextInt(255);
+                    int b = rand.nextInt(255);
+                    int Drawr = rand.nextInt(255);
+					int Drawg = rand.nextInt(255);
+					int Drawb = rand.nextInt(255);
+                    if (evt.getKeyChar() == 'r') {
+                        figs.add(new Rect(x,y, w,h, r,g,b, Drawr,Drawg,Drawb));
+                    }
+					else if (evt.getKeyChar() == 'e') {
+                        figs.add(new Ellipse(x,y, w,h, r,g,b, Drawr,Drawg,Drawb));
+					}
+					else if (evt.getKeyChar() == 'q') {
+						int x1 = rand.nextInt(350);
+						int y1 = rand.nextInt(350);
+						int ctrx = rand.nextInt(350);
+						int ctry = rand.nextInt(350);
+						int x2 = rand.nextInt(350);
+						int y2 = rand.nextInt(350);
+						figs.add(new QuadCurve(x1,y1, ctrx,ctry, x2,y2, r,g,b, Drawr,Drawg,Drawb));
+					}
+					repaint();
+                }
+            }
+        );
+
+        this.setTitle("Lista de Figuras");
+        this.setSize(350, 350);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        for (Figure fig: this.figs) {
+            fig.paint(g);
+        }
+    }
+}
---
+++ 2.Classes/6.1/figures/Ellipse.java
@@ -0,0 +1,38 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.*;
+
+public class Ellipse extends Figure {
+    private int x, y;
+    private int w, h;
+	private int r, g, b;
+	private int Drawr, Drawg, Drawb;
+	
+    public Ellipse (int x, int y, int w, int h, int r, int g, int b, int Drawr, int Drawg, int Drawb) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.Drawr = Drawr;
+		this.Drawg = Drawg;
+		this.Drawb = Drawb;
+    }
+
+    private void print () {
+        System.out.format("Ellipse de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(new Color(this.r,this.g,this.b));
+		g2d.fillOval(this.x,this.y, this.w,this.h);
+        g2d.drawOval(this.x,this.y, this.w,this.h);
+		g2d.setColor(new Color(this.Drawr,this.Drawg,this.Drawb));
+		g2d.drawOval(this.x,this.y, this.w,this.h);
+    }
+}

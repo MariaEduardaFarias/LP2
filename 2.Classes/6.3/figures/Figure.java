80231efbada508de64bd1109d628aea5185b1242
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-04-02 00:05:24 -0300

    Update Figure.java

--- 2.Classes/6.3/figures/Figure.java
+++ 2.Classes/6.3/figures/Figure.java
@@ -3,8 +3,8 @@ package figures;
 import java.awt.Graphics;
 
 public abstract class Figure {
-	int x, y;
-	int r, g, b;
-	int Drawr, Drawg, Drawb;
-	public abstract void paint (Graphics g);
+    int x, y;
+    int r, g, b;
+    int Drawr, Drawg, Drawb;
+    public abstract void paint (Graphics g);
 }

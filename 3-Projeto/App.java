b93ede570c2132d83fd330887170b534da8e171f
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-04-11 23:41:33 -0300

    Update App.java

--- 3-Projeto/App.java
+++ 3-Projeto/App.java
@@ -29,35 +29,35 @@ class ListFrame extends JFrame {
         this.addKeyListener (
             new KeyAdapter() {
                 public void keyPressed (KeyEvent evt) {
-					int x = rand.nextInt(350);
+		    int x = rand.nextInt(350);
                     int y = rand.nextInt(350);
                     int w = rand.nextInt(50);
                     int h = rand.nextInt(50);
-					int ctrx = rand.nextInt(350);
-					int ctry = rand.nextInt(350);
-					int x2 = rand.nextInt(350);
-					int y2 = rand.nextInt(350);
-					int AngleI = rand.nextInt(180);
-					int AngleF = rand.nextInt(280);
-					int r = rand.nextInt(255);
+		    int ctrx = rand.nextInt(350);
+		    int ctry = rand.nextInt(350);
+		    int x2 = rand.nextInt(350);
+		    int y2 = rand.nextInt(350);
+		    int AngleI = rand.nextInt(180);
+		    int AngleF = rand.nextInt(280);
+		    int r = rand.nextInt(255);
                     int g = rand.nextInt(255);
                     int b = rand.nextInt(255);
                     int Drawr = rand.nextInt(255);
-					int Drawg = rand.nextInt(255);
-					int Drawb = rand.nextInt(255);
+		    int Drawg = rand.nextInt(255);
+		    int Drawb = rand.nextInt(255);
                     if (evt.getKeyChar() == 'r') {
                         figs.add(new Rect(x,y, w,h, r,g,b, Drawr,Drawg,Drawb));
                     }
-					else if (evt.getKeyChar() == 'e') {
+		    else if (evt.getKeyChar() == 'e') {
                         figs.add(new Ellipse(x,y, w,h, r,g,b, Drawr,Drawg,Drawb));
-					}
-					else if (evt.getKeyChar() == 'q') {
-						figs.add(new QuadCurve(x,y, ctrx,ctry, x2,y2, r,g,b, Drawr,Drawg,Drawb));
-					}
-					else if (evt.getKeyChar() == 'a') {
-						figs.add(new Arc(x,y, w,h, AngleI,AngleF, r,g,b, Drawr,Drawg,Drawb));
-					}
-					repaint();
+		    }
+		    else if (evt.getKeyChar() == 'q') {
+		        figs.add(new QuadCurve(x,y, ctrx,ctry, x2,y2, r,g,b, Drawr,Drawg,Drawb));
+		    }
+		    else if (evt.getKeyChar() == 'a') {
+		        figs.add(new Arc(x,y, w,h, AngleI,AngleF, r,g,b, Drawr,Drawg,Drawb));
+		    }
+		    repaint();
                 }
             }
         );

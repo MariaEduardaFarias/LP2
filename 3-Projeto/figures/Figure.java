ac6232834487032dd479dd3b664bd1a35ce6084d
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-04-11 23:49:41 -0300

    Update Figure.java

--- 3-Projeto/figures/Figure.java
+++ 3-Projeto/figures/Figure.java
@@ -3,25 +3,25 @@ package figures;
 import java.awt.Graphics;
 
 public abstract class Figure {
-	public int x, y;
-	public int r, g, b;
-	public int Drawr, Drawg, Drawb;
+    public int x, y;
+    public int r, g, b;
+    public int Drawr, Drawg, Drawb;
 	
-	public Figure (int x, int y, int r, int g, int b, int Drawr, int Drawg, int Drawb){
-		this.x = x;
+    public Figure (int x, int y, int r, int g, int b, int Drawr, int Drawg, int Drawb){
+        this.x = x;
         this.y = y;
-		this.r = r;
-		this.g = g;
-		this.b = b;
-		this.Drawr = Drawr;
-		this.Drawg = Drawg;
-		this.Drawb = Drawb;
-	}
+        this.r = r;
+        this.g = g;
+	this.b = b;
+	this.Drawr = Drawr;
+	this.Drawg = Drawg;
+	this.Drawb = Drawb;
+    }
 	
-	public void drag (int dx, int dy) {
+    public void drag (int dx, int dy) {
         this.x += dx;
         this.y += dy;
     }
 	
-	public abstract void paint (Graphics g);
+    public abstract void paint (Graphics g);
 }

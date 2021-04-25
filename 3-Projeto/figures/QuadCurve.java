b9e10770bcf2fbe3c2263b1a838c396ac774d257
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-04-25 00:15:12 -0300

    Update QuadCurve.java

--- 3-Projeto/figures/QuadCurve.java
+++ 3-Projeto/figures/QuadCurve.java
@@ -6,14 +6,14 @@ import java.awt.geom.QuadCurve2D.Double;
 
 public class QuadCurve extends Figure {
     private int ctrx, ctry;
-	private int x2, y2;
+    private int x2, y2;
 
-    public QuadCurve (int x, int y, int ctrx, int ctry, int x2, int y2,int r, int g, int b, int Drawr, int Drawg, int Drawb) {
-        super(x,y, r,g,b, Drawr,Drawg,Drawb);
+    public QuadCurve (int x, int y, int ctrx, int ctry, int x2, int y2, int w, int h, int r, int g, int b, int Drawr, int Drawg, int Drawb) {
+        super(x,y, w,h, r,g,b, Drawr,Drawg,Drawb);
         this.ctrx = ctrx;
         this.ctry = ctry;
-		this.x2 = x2;
-		this.y2 = y2;
+	this.x2 = x2;
+	this.y2 = y2;
     }
 
     private void print () {
@@ -23,10 +23,10 @@ public class QuadCurve extends Figure {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(new Color(this.r,this.g,this.b));
-		g2d.fill(new QuadCurve2D.Double(this.x,this.y, this.ctrx,this.ctry,this.x2,this.y2));
+	g2d.setColor(new Color(this.r,this.g,this.b));
+	g2d.fill(new QuadCurve2D.Double(this.x,this.y, this.ctrx,this.ctry,this.x2,this.y2));
         g2d.draw(new QuadCurve2D.Double(this.x,this.y, this.ctrx,this.ctry,this.x2,this.y2));
-		g2d.setColor(new Color(this.Drawr,this.Drawg,this.Drawb));
+	g2d.setColor(new Color(this.Drawr,this.Drawg,this.Drawb));
         g2d.draw(new QuadCurve2D.Double(this.x,this.y, this.ctrx,this.ctry,this.x2,this.y2));
     }
 }

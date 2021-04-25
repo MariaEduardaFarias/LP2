aa365fe5c57ff83f61f162bec95402d2b9fd3d75
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-04-24 22:10:18 -0300

    Update Ellipse.java

--- 3-Projeto/figures/Ellipse.java
+++ 3-Projeto/figures/Ellipse.java
@@ -4,12 +4,9 @@ import java.awt.*;
 import java.awt.geom.*;
 
 public class Ellipse extends Figure {
-    private int w, h;
 	
     public Ellipse (int x, int y, int w, int h, int r, int g, int b, int Drawr, int Drawg, int Drawb) {
-        super(x,y, r,g,b, Drawr,Drawg,Drawb);
-        this.w = w;
-        this.h = h;
+        super(x,y, w,h, r,g,b, Drawr,Drawg,Drawb);
     }
 
     private void print () {
@@ -19,10 +16,10 @@ public class Ellipse extends Figure {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(new Color(this.r,this.g,this.b));
-		g2d.fillOval(this.x,this.y, this.w,this.h);
+	g2d.setColor(new Color(this.r,this.g,this.b));
+	g2d.fillOval(this.x,this.y, this.w,this.h);
         g2d.drawOval(this.x,this.y, this.w,this.h);
-		g2d.setColor(new Color(this.Drawr,this.Drawg,this.Drawb));
-		g2d.drawOval(this.x,this.y, this.w,this.h);
+	g2d.setColor(new Color(this.Drawr,this.Drawg,this.Drawb));
+	g2d.drawOval(this.x,this.y, this.w,this.h);
     }
 }

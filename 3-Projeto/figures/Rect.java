1189ed1995a5e48301f94c153a7ddd1f1472dd82
MariaEduardaFarias <79468236+MariaEduardaFarias@users.noreply.github.com>
2021-05-22 17:06:04 -0300

    Update Rect.java

--- 3-Projeto/figures/Rect.java
+++ 3-Projeto/figures/Rect.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class Rect extends Figure {
 
-    public Rect (int x, int y, int w, int h, int r, int g, int b, int Drawr, int Drawg, int Drawb) {
+	public Rect (int x, int y, int w, int h, int r, int g, int b, int Drawr, int Drawg, int Drawb) {
         super(x,y, w,h, r,g,b, Drawr,Drawg,Drawb);
     }
 
@@ -13,10 +13,16 @@ public class Rect extends Figure {
             this.w, this.h, this.x, this.y);
     }
 
-    public void paint (Graphics g) {
+    public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
-	g2d.setColor(new Color(this.r,this.g,this.b));
-	g2d.fillRect(this.x,this.y, this.w,this.h);
+		
+		if (focused) {
+			g2d.setPaint(Color.RED);
+			g2d.drawRect(this.x-3, this.y-3, this.w+6, this.h+6);
+		}
+		
+		g2d.setColor(new Color(this.r,this.g,this.b));
+		g2d.fillRect(this.x,this.y, this.w,this.h);
         g2d.setColor(new Color(this.Drawr,this.Drawg,this.Drawb));
         g2d.drawRect(this.x,this.y, this.w,this.h);
     }
